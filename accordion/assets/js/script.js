359b2b939b5a6e6729aed2e76d625c0f1a4608ab
TheoPierne <theo.pierne2@gmail.com>
2022-07-14 19:20:12 +0200

    Remove unused CSS and some JS modifications

--- accordion/assets/js/script.js
+++ accordion/assets/js/script.js
@@ -1,15 +1,15 @@
 const $accordions = document.querySelectorAll('.accordion');
-let activeAccordion = [...$accordions].filter(e => e.classList.contains('is-active'))[0];
+let $activeAccordion = [...$accordions].filter($e => $e.classList.contains('is-active'))[0];
 
-$accordions.forEach(e => {
-	e.onclick = () => {
-		if(e.classList.contains('is-active')){
-			e.classList.remove('is-active');
-			activeAccordion = null;
+$accordions.forEach($e => {
+	$e.onclick = () => {
+		if($e.classList.contains('is-active')){
+			$e.classList.remove('is-active');
+			$activeAccordion = null;
 		}else{
-			activeAccordion && activeAccordion.classList.remove('is-active');
-			e.classList.add('is-active');
-			activeAccordion = e;
+			$activeAccordion && $activeAccordion.classList.remove('is-active');
+			$e.classList.add('is-active');
+			$activeAccordion = $e;
 		}
 	}
 });
